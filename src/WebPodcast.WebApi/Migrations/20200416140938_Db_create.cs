0fbdd5f69495db43ca07e386e025465b4fa24f0b
Shelgas <akozlov1997@gmail.com>
2020-04-16 17:16:50 +0300

    Created a project structure

---
+++ src/WebPodcast.WebApi/Migrations/20200416140938_Db_create.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebPodcast.WebApi.Migrations
+{
+    public partial class Db_create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
